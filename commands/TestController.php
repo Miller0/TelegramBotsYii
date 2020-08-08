7ea9c51ee95fae353e0f69b274f43d571a2cf584
Miller0 <mr.milller2017.11@gmail.com>
2020-08-08 08:32:37 +0300

    *

--- commands/TestController.php
+++ commands/TestController.php
@@ -4,8 +4,6 @@
 namespace app\commands;
 
 
-
-
 use app\models\generated\Messages;
 use Longman\TelegramBot\Request;
 use Longman\TelegramBot\Telegram;
@@ -14,17 +12,20 @@ use yii\console\Controller;
 class TestController extends Controller
 {
 
-public function actionTest()
-{
-
-    $text =  Messages::find()->select(['text'])->where(['chatId'=>42])->orderBy(['create' => SORT_DESC])->asArray()->one();
-    $telegram = new Telegram('1178137066:AAFoghTqNgwd1RdBCmA7k8H6ZpeB4L9T8gg', '1');
-                $result = Request::sendMessage([
+    public function actionTest()
+    {
+        for ($i = 0; $i < 5; $i++)
+        {
+            $text = Messages::find()->select(['text'])->where(['chatId' => 42])->orderBy(['create' => SORT_DESC])->asArray()->one();
+            $telegram = new Telegram('1178137066:AAFoghTqNgwd1RdBCmA7k8H6ZpeB4L9T8gg', '1');
+            $result = Request::sendMessage([
                 'chat_id' => -1001289632263,
-                'text'    => $text['text'],
+                'text' => $text['text'],
             ]);
-                return true;
-}
+            sleep(5);
+        }
+        return true;
+    }
 
 
 }
